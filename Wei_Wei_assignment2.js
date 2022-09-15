8a1fa28fc0ceed51d18f5a8ba030f29b419cf098
Mac <mac@Macs-MBP.router.taotronics.com>
2022-09-14 22:08:42 -0700

    problem solved

--- Wei_Wei_assignment2.js
+++ Wei_Wei_assignment2.js
@@ -11,6 +11,29 @@ const itemsObject = [
   { quantity: 5, price: 400 },
 ];
 
+//double the quantity aned price
+const ans1 = itemsObject.map((a) => {
+  return { ...a };
+});
+
+ans1.forEach((item) => {
+  item.quantity *= 2;
+  item.price *= 2;
+});
+console.log(ans1);
+
+//item quantiy > 2 and price > 300
+const ans2 = itemsObject.filter(
+  (item) => item.price > 300 && item.quantity > 2
+);
+console.log(ans2);
+
+//calculate the total value of the items
+const ans3 = itemsObject.reduce((sum, item) => {
+  return sum + item.quantity * item.price;
+}, 0);
+console.log(ans3);
+
 /*
   Question 2
   Given the string, implement a function to remove all the non-alphabet characters and extra space in the string and convert the string to all lowercase.
@@ -22,9 +45,16 @@ const string =
 const expectedReturnString =
   "perhaps the easiest to understand case for reduce is to return the sum of all the elements in an array";
 
+let newStr = string
+  .replace(/\s+/g, " ")
+  .replace(/[^A-Za-z0-9]/g, " ")
+  .trim()
+  .toLowerCase();
+console.log(newStr);
+
 /*
   Question 3
-  Implement a function to merge two arrays of objects on uuid, but first has uuid and name, second has uuid and role. With the not existing property, fill with null. Sort according to uuid after merge.
+  Implement a function to merge two arrays of objects on uuid, but first has uuid and name, second has uuid and role. With the not existing property, fill with null. Sort according to uuid after merge.  
   */
 
 const first = [
@@ -48,3 +78,31 @@ const expectedReturnArray = [
   { uuid: 5, role: null, name: "test5" },
   { uuid: 6, role: "pm", name: null },
 ];
+
+const mergeTwoArr = (arr1, arr2) => {
+  const map = new Map();
+  for (let i = 0; i < arr1.length; i++) {
+    map.set(arr1[i].uuid, [arr1[i].name]);
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    map.has(arr2[j].uuid)
+      ? map.get(arr2[j].uuid).push(arr2[j].role)
+      : map.set(arr2[j].uuid, [null, arr2[j].role]);
+  }
+  //console.log(map);
+  const mergedArr = [];
+  let keys = Array.from(map.keys());
+  //console.log(keys);
+  for (let i = 0; i < keys.length; i++) {
+    mergedArr[i] = {
+      uuid: keys[i],
+      name: map.get(keys[i])[0],
+      role: map.get(keys[i])[1],
+    };
+  }
+  mergedArr.sort((a, b) => a.uuid - b.uuid);
+  //console.log(mergedArr);
+  return mergedArr;
+};
+
+mergeTwoArr(first, second);
