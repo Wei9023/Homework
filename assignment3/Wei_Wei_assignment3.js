c2ad51a5ccc7f3c589430e12bb1e571285fea8e6
Mac <mac@Macs-MBP.router.taotronics.com>
2022-09-19 13:18:31 -0700

    html complete

--- assignment3/Wei_Wei_assignment3.js
+++ assignment3/Wei_Wei_assignment3.js
@@ -33,12 +33,80 @@ const tableInfo = {
   ],
 };
 
+const bd = document.querySelector("body");
+
+const generateTable = () => {
+  const tb = document.createElement("table");
+  const tHead = document.createElement("thead");
+  const tBody = document.createElement("tbody");
+
+  //Ceating columns
+  const column = document.createElement("tr");
+  for (let i = 0; i < tableInfo.tableHeader.length; i++) {
+    let item = document.createElement("th");
+    item.textContent = tableInfo.tableHeader[i];
+    //console.log(item);
+    column.appendChild(item);
+  }
+  //console.log(column);
+  tHead.appendChild(column);
+  console.log(tHead);
+
+  //creating all cells
+  for (let i = 0; i < tableInfo.tableContent.length; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < tableInfo.tableHeader.length; j++) {
+      const cell = document.createElement("td");
+      cell.textContent = Object.values(tableInfo.tableContent[i])[j];
+      //console.log(cell);
+      row.appendChild(cell);
+    }
+    console.log(row);
+    tBody.appendChild(row);
+  }
+  tb.appendChild(tHead);
+  tb.appendChild(tBody);
+  console.log(tb);
+  bd.appendChild(tb);
+};
+
+const tableBtn = document.querySelector("#btn");
+tableBtn.addEventListener("click", generateTable);
+
 /*
   Question 2
   Given the array and generate order list and unordered list dynamically as following:
   */
 
 const list = ["HTML", "JavaScript", "CSS", "React", "Redux", "Java"];
+
+//generate ordered list
+const generateOrderedList = () => {
+  const orderedList = document.createElement("ol");
+  for (let i = 0; i < list.length; i++) {
+    let listItem = document.createElement("li");
+    listItem.textContent = list[i];
+    orderedList.appendChild(listItem);
+  }
+  bd.appendChild(orderedList);
+};
+
+const olBtn = document.querySelector("#olbtn");
+olBtn.addEventListener("click", generateOrderedList);
+
+//generate unordered list
+const generateUnorderedList = () => {
+  const unorderedList = document.createElement("ul");
+  for (let i = 0; i < list.length; i++) {
+    let listItem = document.createElement("li");
+    listItem.textContent = list[i];
+    unorderedList.appendChild(listItem);
+  }
+  bd.appendChild(unorderedList);
+};
+
+const ulBtn = document.querySelector("#ulbtn");
+ulBtn.addEventListener("click", generateUnorderedList);
 
 /*
   Question 3
@@ -54,3 +122,18 @@ const dropDownList = [
   { value: "dalyCity", content: "Daly City" },
   { value: "sanJose", content: "San Jose" },
 ];
+
+const generateDropDownList = () => {
+  const dropDown = document.createElement("select");
+  for (let i = 0; i < dropDownList.length; i++) {
+    let options = document.createElement("option");
+    options.textContent = dropDownList[i].content;
+    let att = document.createAttribute("value");
+    att.textContent = dropDownList[i].value;
+    dropDown.appendChild(options);
+  }
+  bd.appendChild(dropDown);
+};
+
+const dropDownBtn = document.querySelector("#dropdownbtn");
+dropDownBtn.addEventListener("click", generateDropDownList);
